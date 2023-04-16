af4989272cf651752cfdf3a5de3e323ed50c9b50
Vasiko Mtsituri <vasiko.mtsituri@makingscience.com>
2023-04-16 11:21:04 +0400

    feat(day2): implement header extraction from RDD and to use just the data of it. Split and take the columns we want now available in function

--- GCP_DATAPROC_DEMO_WITH_SCALA/src/Main.scala
+++ GCP_DATAPROC_DEMO_WITH_SCALA/src/Main.scala
@@ -1,7 +1,8 @@
 package src
 
 import org.apache.spark.sql.SparkSession
-object Main extends App{
+
+object Main extends App {
   val spark = SparkSession.builder()
     .master("local[1]")
     .appName("SparkByExample")
@@ -10,7 +11,14 @@ object Main extends App{
   val filePath = "/home/vaso/Downloads/nyc_tlc_yellow_trips_2018_subset_1.csv"
   val fileRDD = spark.sparkContext.textFile(filePath)
 
-  println("Number of rows: " + fileRDD.count())
+  val header = fileRDD.first()
 
-  fileRDD.take(20).foreach(println)
+  val fileRDDWithoutHeader = fileRDD.filter(_ != header)
+
+  println(header)
+
+  val subsetOfNYCRides = fileRDDWithoutHeader.map(line => {
+    val colArray = line.split(",")
+    (colArray(0), colArray(2), colArray(3))
+  }).take(10).foreach(println)
 }
